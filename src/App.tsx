aad908410c8a338ca2438aed7fadfd310d149626
YashKharcheTekF <yash.kharche@tekframeworks.com>
2025-11-22 12:41:42 +0530

    test new ip

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Setup CICD</h1>
+      <h1>Setup CICD (2)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
